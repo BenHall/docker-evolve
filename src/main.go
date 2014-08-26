25af3ecaf14a4f7a389536dd8387e6db74230378
Ben Hall <ben@benhall.me.uk>
2014-08-26 16:09:18 +0100

    Parse filename from command line arguments

--- src/main.go
+++ src/main.go
@@ -2,12 +2,25 @@ package main
 
 import (
     "fmt"
+    "os"
+    "flag"
     Parser "./server_spec_parser"
 )
 
+var serverConfigFile = flag.String("config", "", "Server Configuration File")
+var serverTargetIpAddress = flag.String("ip", "", "IP of Target Server")
+
 func main() {
+    flag.Parse()
+
+    if(*serverConfigFile == "") {
+        fmt.Fprintf(os.Stderr, "Error: No server configuration provided \n")
+        os.Exit(1)
+    }
+
+
     ServerSpec := &Parser.ServerSpec{}
-    err := ServerSpec.Parse("./../examples/server_node_nginx.json")
+    err := ServerSpec.Parse(*serverConfigFile)
     if err != nil { panic(err) }
 
 
